95e6f47cbf70dd085ffaeefc34b4109a248594b7
ASteinheiser <me@iamandrew.io>
2025-04-01 18:54:07 -0700

    add PWA support and generate relevant image files

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,25 +1,45 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-const phasermsg = () => {
-  return {
-    name: 'phasermsg',
-    buildStart() {
-      process.stdout.write(`Building for production...\n`);
-    },
-    buildEnd() {
-      const line = '---------------------------------------------------------';
-      const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
-      process.stdout.write(`${line}\n${msg}\n${line}\n`);
-
-      process.stdout.write(`✨ Done ✨\n`);
-    },
-  };
-};
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: './',
-  plugins: [react(), phasermsg()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Demo Game',
+        short_name: 'Demo Game',
+        theme_color: '#0f0f0f',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   logLevel: 'warning',
   build: {
     rollupOptions: {
