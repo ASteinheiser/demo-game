b11f0f7a99096f79a759e2021560cdaa3e920eb1
ASteinheiser <me@iamandrew.io>
2025-04-01 23:56:56 -0700

    finish setting up eslint with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,25 @@ import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { prettier },
+    extends: [eslintConfigPrettier],
+    rules: { 'prettier/prettier': 'error' },
+  },
+  {
+    files: ['**/*.{jsx,tsx}'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ]);
