6cdf5fffc5c8b32105e1e77266c4d93d292aff3d
ASteinheiser <me@iamandrew.io>
2025-04-16 17:52:13 -0700

    send movement events to the server

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -9,6 +9,16 @@ export class Game extends Scene {
   background: Phaser.GameObjects.Image;
   gameText: Phaser.GameObjects.Text;
   client: Client;
+  room: any;
+  playerEntities: Record<string, Phaser.GameObjects.Image>;
+
+  cursorKeys?: Phaser.Types.Input.Keyboard.CursorKeys;
+  inputPayload = {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+  };
 
   constructor() {
     super('Game');
@@ -16,6 +26,10 @@ export class Game extends Scene {
     if (!this.client) {
       this.client = new Client(GAME_SERVER_URL);
     }
+  }
+
+  preload() {
+    this.cursorKeys = this.input.keyboard?.createCursorKeys();
   }
 
   async create() {
@@ -26,15 +40,28 @@ export class Game extends Scene {
     this.background.setAlpha(0.5);
 
     try {
-      const room = await this.client.joinOrCreate('my_room', {
-        /* options */
-      });
-      console.log('joined successfully', room);
+      if (!this.room) {
+        this.room = await this.client.joinOrCreate('my_room', {});
+      }
+      console.log('joined successfully', this.room);
     } catch (e) {
       console.error('join error', e);
     }
 
+    console.log({ room: this.room });
+
     EventBus.emit('current-scene-ready', this);
+  }
+
+  update(time: number, delta: number): void {
+    // skip loop if not connected with room yet
+    if (!this.room || !this.cursorKeys) return;
+
+    this.inputPayload.left = this.cursorKeys.left.isDown;
+    this.inputPayload.right = this.cursorKeys.right.isDown;
+    this.inputPayload.up = this.cursorKeys.up.isDown;
+    this.inputPayload.down = this.cursorKeys.down.isDown;
+    this.room.send('movement', this.inputPayload);
   }
 
   changeScene() {
