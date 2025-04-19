d61572f420f328b16009fbd32b5f23b2a4638a16
ASteinheiser <me@iamandrew.io>
2025-04-19 12:03:50 -0700

    created StartGameForm to take in a custom username

---
+++ src/components/StartGameForm.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './ui/dialog';
+
+interface StartGameFormProps {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  onSubmit: (args: { username: string }) => void;
+}
+
+export const StartGameForm = ({ isOpen, onOpenChange, onSubmit }: StartGameFormProps) => {
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = () => {
+    const trimmedUsername = username.trim();
+    if (trimmedUsername === '') return;
+
+    onSubmit({ username: trimmedUsername });
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Join a game</DialogTitle>
+          <DialogDescription>Enter your username to join a game</DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Username
+            </Label>
+            <Input
+              id="name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit" onClick={handleSubmit}>
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
