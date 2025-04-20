5e64d643d4c46f7b499ef81b1ba7d36a521e803d
ASteinheiser <me@iamandrew.io>
2025-04-19 21:51:26 -0700

    first pass at abstracting Player game object

---
+++ src/game/objects/Player.ts
@@ -0,0 +1,68 @@
+export class Player {
+  entity: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  nameText: Phaser.GameObjects.Text;
+
+  constructor(
+    entity: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody,
+    nameText: Phaser.GameObjects.Text
+  ) {
+    this.entity = entity;
+    this.nameText = nameText;
+
+    this.entity.anims.create({
+      key: 'playerIdle',
+      frames: this.entity.anims.generateFrameNumbers('player', { frames: [0] }),
+      frameRate: 100,
+      repeat: 0,
+    });
+    this.entity.anims.create({
+      key: 'playerWalk',
+      frames: this.entity.anims.generateFrameNumbers('player', { frames: [2, 3, 4, 1] }),
+      frameRate: 8,
+      repeat: -1,
+    });
+    // actual punch frame is 0.375s after start of animation
+    this.entity.anims.create({
+      key: 'playerPunch',
+      frames: this.entity.anims.generateFrameNumbers('player', { frames: [5, 6, 7, 8, 5] }),
+      frameRate: 8,
+      repeat: 0,
+    });
+  }
+
+  move({ x, y }: { x: number; y: number }) {
+    const isMoving = this.entity.x !== x || this.entity.y !== y;
+
+    this.entity.setFlipX(this.entity.x > x);
+
+    this.entity.x = x;
+    this.entity.y = y;
+    this.nameText.x = x;
+    this.nameText.y = y;
+
+    if (!isMoving) {
+      if (this.entity.anims.isPlaying && this.entity.anims.currentAnim?.key === 'playerPunch') {
+        return;
+      }
+      this.entity.play('playerIdle');
+    } else if (
+      this.entity.anims.isPlaying &&
+      ['playerPunch', 'playerWalk'].includes(this.entity.anims.currentAnim?.key ?? '')
+    ) {
+      return;
+    } else {
+      this.entity.play('playerWalk');
+    }
+  }
+
+  attack() {
+    if (this.entity.anims.isPlaying && this.entity.anims.currentAnim?.key === 'playerPunch') return;
+
+    this.entity.anims.play('playerPunch');
+  }
+
+  destroy() {
+    this.entity.destroy();
+    this.nameText.destroy();
+  }
+}
