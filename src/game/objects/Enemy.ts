e6d7431a38856185c376d5fa3be65f674b6c5de4
ASteinheiser <me@iamandrew.io>
2025-04-22 23:05:21 -0700

    draw enemy hitboxes

--- src/game/objects/Enemy.ts
+++ src/game/objects/Enemy.ts
@@ -1,8 +1,14 @@
+// hardcode the enemy size for now
+const HITBOX_SIZE = 64;
+
 export class Enemy {
   entity: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  hitbox: Phaser.GameObjects.Rectangle;
 
   constructor(scene: Phaser.Scene, x: number, y: number) {
     this.entity = scene.physics.add.sprite(x, y, 'enemy');
+    this.hitbox = scene.add.rectangle(x, y, HITBOX_SIZE, HITBOX_SIZE);
+    this.hitbox.setStrokeStyle(1, 0xff00ff);
   }
 
   destroy() {
